ef165915410282e05ae2555b088189408fdb0575
Sarun Intaralawan <sarun.intaralawan@gmail.com>
2017-10-10 11:19:26 +0700

    Stage 1

---
+++ src/app/app.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  template: `<div>{{text}}</div><input type="text" [(ngModel)]="text">`
+})
+export class AppComponent implements OnInit {
+  text: string;
+
+  constructor() {
+    this.text = 'Hello';
+  }
+
+  ngOnInit() { }
+}
---
+++ src/app/app.module.ts
@@ -0,0 +1,11 @@
+import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+
+import { AppComponent } from './app.component';
+
+@NgModule({
+  imports: [FormsModule],
+  declarations: [AppComponent],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
